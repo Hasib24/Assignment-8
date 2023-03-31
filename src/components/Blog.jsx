65f483f93f17c3943e9a7ac89a4edac47c94a6ed
Hasib24 <hasibislam1488@gmail.com>
2023-04-01 00:21:47 +0600

    update header responsiveness

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,7 +4,7 @@ import Post from './Post';
 const Blog = ({posts, bookMarkHandler, markAsReadHandler}) => {
     // console.log(posts);
     return (
-        <div className='col-span-8 border'>
+        <div className='col-span-8 border-r border-l'>
             {posts.map(post => <Post key={post.id} post={post} bookMarkHandler={bookMarkHandler} markAsReadHandler={markAsReadHandler}></Post>)}
         </div>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,16 @@ import proPic from './../img/Ellipse 1.png'
 
 const Header = () => {
     return (
-        <div className='bg-slate-800'>
-            <div className='container flex justify-between bg-slate-500 mx-auto px-6 h-16 items-center'>
-                <h1 className='text-3xl'>Knowledge Cafe</h1>
-                <div className='flex items-center'>
+        <div className=''>
+            <div className='container md:flex justify-between border-b mx-auto  py-8 items-center'>
+                <h1 className='text-3xl font-bold text-center'>Knowledge Cafe</h1>
+                <div className='flex items-center justify-center border'>
                     <div>
-                        <a className='mr-5' href="/abc">Sign In</a>
-                        <a className='mr-5' href="/abc">Blog</a>
+                        <a className='m-5' href="/abc">Sign In</a>
+                        <a className='m-5' href="/abc">Blog</a>
+                        <a className='m-5 md:hidden visible' href="/abc">Profile</a>
                     </div>
-                    <img className='w-12' src={proPic} alt="" />
+                    <img className='w-12 cursor-pointer invisible md:visible' src={proPic} alt="" />
                 </div>
             </div>
 
