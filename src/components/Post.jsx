e5522b3134f01e825e161c2e5ef2624ba3f7b1f6
Hasib24 <hasibislam1488@gmail.com>
2023-04-01 05:08:52 +0600

    update data.json file again

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,7 @@ import { FaRegBookmark } from 'react-icons/fa';
 
 const Post = ({post, bookMarkHandler, markAsReadHandler}) => {
     // console.log(post);
-    let {id, author, authorPic, date, requiredTime, title, img, hashTag} = post;
+    let {id, authorName, authorImage, publishDate, readTime, blogTitle, blogCoverImage, hashTag} = post;
     let [hashTag1, hashTag2] = hashTag;
 
     const bookMarkBtn = (id, title) =>{
@@ -15,30 +15,30 @@ const Post = ({post, bookMarkHandler, markAsReadHandler}) => {
     }
     return (
         <div className='p-5'>
-            <img className='rounded' src={img} alt="" />
+            <img className='rounded' src={blogCoverImage} alt="" />
             <div className='flex justify-between items-center py-5 px-2 my-5 border'>
                 {/* header div of each post */}
                 <div className='flex items-center'>
-                    <img className='w-14 rounded-full mr-3' src={authorPic} alt="" />
+                    <img className='w-14 rounded-full mr-3' src={authorImage} alt="" />
                     <div>
-                        <div className='font-bold' >{author}</div>
-                        <div className='text-slate-500'>{date}</div>
+                        <div className='font-bold' >{authorName}</div>
+                        <div className='text-slate-500'>{publishDate}</div>
                     </div>
                 </div>
                 {/* time required and bookmark  */}
                 <div className='flex'>
-                    <div className='text-slate-500 mr-4'>{requiredTime} min </div>
-                    <button  onClick={()=>{bookMarkBtn(id, title)}}><FaRegBookmark/></button>
+                    <div className='text-slate-500 mr-4'>{readTime} min </div>
+                    <button  onClick={()=>{bookMarkBtn(id, blogTitle)}}><FaRegBookmark/></button>
                 </div>
             </div>
             <div>
-                <h1 className='text-3xl font-bold'>{title}</h1>
+                <h1 className='text-3xl font-bold'>{blogTitle}</h1>
                 <p className='my-5'>
                     <span className='px-3 py-1 hover:underline cursor-pointer mr-2 text-slate-500 bg-slate-50 border rounded-3xl'>#{hashTag1} </span>
                     <span className='px-3 py-1 hover:underline cursor-pointer mr-2 text-slate-500 bg-slate-50 border rounded-3xl'>#{hashTag2}</span>
                 </p>
             </div>
-            <button className='font-bold text-blue-500 underline' onClick={()=>{markAsReadBtn(requiredTime)}} >Mark as read</button>
+            <button className='font-bold text-blue-500 underline' onClick={()=>{markAsReadBtn(readTime)}} >Mark as read</button>
             
         </div>
     );
