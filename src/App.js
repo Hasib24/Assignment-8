2c1fa90bb09615aaa3173067be136a01421cd860
Hasib24 <hasibislam1488@gmail.com>
2023-04-01 05:31:58 +0600

    initialize blog component for qn and ans

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import BlogForQnAndAns from './components/BlogForQnAndAns'
 function App() {
   return (
     <div className='container mx-auto'>
       <Header></Header>
       <Main></Main>
+      <BlogForQnAndAns></BlogForQnAndAns>
+      
     </div>
   );
 }
---
+++ src/components/BlogForQnAndAns.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogForQnAndAns = () => {
+    return (
+        <div>
+            hellow from blog 
+        </div>
+    );
+};
+
+export default BlogForQnAndAns;
